6f2e2e8e972e378586f61f9229cb553f234f869d
Lucky <lucky3028@users.noreply.github.com>
2021-04-11 20:03:52 +0900

    [Add] アカウントとリージョンを明示指定

--- bin/auto-link-ec2-ip.ts
+++ bin/auto-link-ec2-ip.ts
@@ -5,4 +5,9 @@ import { AutoLinkEc2IpStack } from '../lib/auto-link-ec2-ip-stack';
 
 const app = new cdk.App();
 // eslint-disable-next-line no-new
-new AutoLinkEc2IpStack(app, 'AutoLinkEc2IpStack');
+new AutoLinkEc2IpStack(app, 'AutoLinkEc2IpStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
