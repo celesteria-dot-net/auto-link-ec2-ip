5a16edd266369197f79a8b4eba8b6c649fa5fbe1
Lucky <lucky3028@users.noreply.github.com>
2021-04-11 20:08:10 +0900

    [Add] ホストゾーンの定義

--- lib/auto-link-ec2-ip-stack.ts
+++ lib/auto-link-ec2-ip-stack.ts
@@ -1,4 +1,6 @@
+/* eslint-disable no-new */
 import * as cdk from '@aws-cdk/core';
+import * as route53 from '@aws-cdk/aws-route53';
 
 // eslint-disable-next-line import/prefer-default-export
 export class AutoLinkEc2IpStack extends cdk.Stack {
@@ -6,6 +8,9 @@ export class AutoLinkEc2IpStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hostZone = new route53.PublicHostedZone(this, 'DefaultHostZone', {
+      zoneName: 'celesteria.net',
+      comment: 'This hostzone is managed by AWS CDK.',
+    });
   }
 }
