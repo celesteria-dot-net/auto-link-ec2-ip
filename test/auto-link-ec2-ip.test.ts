b48d69d5085e730352aab637234ecad6be7b4771
Lucky <lucky3028@users.noreply.github.com>
2021-04-11 17:21:54 +0900

    Initial commit

---
+++ test/auto-link-ec2-ip.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AutoLinkEc2Ip from '../lib/auto-link-ec2-ip-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new AutoLinkEc2Ip.AutoLinkEc2IpStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
