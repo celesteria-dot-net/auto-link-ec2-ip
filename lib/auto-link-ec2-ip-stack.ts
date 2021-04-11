61bf85f045e3db9719723281d39e9d4406599ef1
Lucky <lucky3028@users.noreply.github.com>
2021-04-11 17:37:22 +0900

    [Fix] lint

--- lib/auto-link-ec2-ip-stack.ts
+++ lib/auto-link-ec2-ip-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 
+// eslint-disable-next-line import/prefer-default-export
 export class AutoLinkEc2IpStack extends cdk.Stack {
+  // eslint-disable-next-line no-useless-constructor
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
--- test/auto-link-ec2-ip.test.ts
+++ test/auto-link-ec2-ip.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as AutoLinkEc2Ip from '../lib/auto-link-ec2-ip-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new AutoLinkEc2Ip.AutoLinkEc2IpStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new AutoLinkEc2Ip.AutoLinkEc2IpStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT,
+    ),
+  );
 });
