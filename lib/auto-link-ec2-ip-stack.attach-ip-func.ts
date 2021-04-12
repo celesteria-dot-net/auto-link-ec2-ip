8cb7cc2fb3cf8fc6fda235e83754390e7494b28e
Lucky <lucky3028@users.noreply.github.com>
2021-04-12 20:55:19 +0900

    [Add] EC2インスタンスのステ変更を受けてIPをレコードに追加する

---
+++ lib/auto-link-ec2-ip-stack.attach-ip-func.ts
@@ -0,0 +1,70 @@
+import fetchInstances from './domains/ec2';
+import changeResourceRecordSets from './domains/route53';
+
+type instanceStateChangeEvent = {
+  version: string;
+  id: string;
+  'detail-type': string;
+  source: string;
+  account: string;
+  time: string;
+  region: string;
+  resources: string[];
+  detail: {
+    'instance-id': string;
+    state: string;
+  };
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const handler = async (event: instanceStateChangeEvent) => {
+  const action = (() => {
+    switch (event.detail.state) {
+      case 'running':
+        return 'Upsert';
+      case 'stopping':
+        return 'Delete';
+      default:
+        return '';
+    }
+  })();
+
+  const query = {
+    InstanceIds: [event.detail['instance-id']],
+  };
+  const outputs = await fetchInstances(query);
+
+  // 一意のインスタンスIDが1つだけ指定されたイベントが発生し、それをもとに取得してきているので、考慮するインスタンスは1つだけでよい
+  const instance = outputs.Reservations?.flatMap(
+    (res) => res.Instances ?? [],
+  )?.find((ins) => ins.Tags?.flatMap((tag) => tag.Key?.includes('hostName')));
+  const subDomain = instance?.Tags?.find((tag) => tag.Key === 'hostName')
+    ?.Value;
+  const ipAddress = instance?.PublicIpAddress;
+
+  if (!action || !instance || !subDomain || !ipAddress) return undefined;
+
+  const hostName = `${subDomain}.celesteria.net.`;
+  const route53Query = {
+    HostedZoneId: process.env.HOSTZONEID,
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: action,
+          ResourceRecordSet: {
+            Name: hostName,
+            Type: 'A',
+            TTL: 60 * 5,
+            ResourceRecords: [
+              {
+                Value: ipAddress,
+              },
+            ],
+          },
+        },
+      ],
+    },
+  };
+
+  return changeResourceRecordSets(route53Query);
+};
