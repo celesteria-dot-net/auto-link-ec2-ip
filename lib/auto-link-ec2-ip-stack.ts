e217a20e0167173471f02a2a39478f2e16680d4d
Lucky <lucky3028@users.noreply.github.com>
2021-04-12 21:07:23 +0900

    [Add] cdkのスタック定義

--- lib/auto-link-ec2-ip-stack.ts
+++ lib/auto-link-ec2-ip-stack.ts
@@ -1,16 +1,48 @@
-/* eslint-disable no-new */
 import * as cdk from '@aws-cdk/core';
-import * as route53 from '@aws-cdk/aws-route53';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { PublicHostedZone } from '@aws-cdk/aws-route53';
+import { Rule } from '@aws-cdk/aws-events';
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
 
 // eslint-disable-next-line import/prefer-default-export
 export class AutoLinkEc2IpStack extends cdk.Stack {
-  // eslint-disable-next-line no-useless-constructor
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const hostZone = new route53.PublicHostedZone(this, 'DefaultHostZone', {
+    const hostZone = new PublicHostedZone(this, 'DefaultHostedZone', {
       zoneName: 'celesteria.net',
-      comment: 'This hostzone is managed by AWS CDK.',
+      comment: 'This hostzone is managed by AWS CDK App.',
     });
+
+    const attachIpFunc = new NodejsFunction(this, 'attach-ip-func', {
+      environment: {
+        HOSTZONEID: hostZone.hostedZoneId,
+      },
+    });
+
+    [
+      new PolicyStatement({
+        resources: ['*'],
+        actions: ['ec2:DescribeInstances', 'ec2:DescribeVpcs'],
+      }),
+      new PolicyStatement({
+        actions: [
+          'route53:CreateHostedZone',
+          'route53:ChangeResourceRecordSets',
+        ],
+        resources: [
+          'arn:aws:route53:::hostedzone/*',
+          `arn:aws:ec2:*:${cdk.Aws.ACCOUNT_ID}:vpc/*`,
+        ],
+      }),
+    ].forEach((policy) => attachIpFunc.addToRolePolicy(policy));
+
+    new Rule(this, 'attachIpFunc-Schedule', {
+      eventPattern: {
+        source: ['aws.ec2'],
+        detailType: ['EC2 Instance State-change Notification'],
+      },
+    }).addTarget(new LambdaFunction(attachIpFunc));
   }
 }
