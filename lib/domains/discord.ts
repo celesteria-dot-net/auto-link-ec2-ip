30f30075d7d9c518483f5d9256f5c5e06c77350f
Lucky <lucky3028@users.noreply.github.com>
2021-04-13 20:53:08 +0900

    [Add] embedのエラー処理をまとめる

--- lib/domains/discord.ts
+++ lib/domains/discord.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Webhook, MessageBuilder } from 'discord-webhook-node';
 import env from '../utils/env';
 
@@ -5,6 +6,14 @@ const webhook = new Webhook(env.DISCORD_TOKEN);
 webhook.setUsername('EC2-Auto-Link-Ip');
 webhook.setAvatar('https://i.gyazo.com/6cfe09c275242cd62647a3f90ca039e6.png');
 
-const sendEmbed = (embed: MessageBuilder): Promise<void> => webhook.send(embed)
+const sendEmbed = (embed: MessageBuilder): Promise<void> =>
+  webhook.send(embed).catch((err) => {
+    console.error('DiscordにEmbedを送信できませんでした');
+    console.error(err);
+  });
 
-export default sendEmbed;
+const defaultEmbed = new MessageBuilder().setTitle(
+  'EC2 Instance Status Change',
+);
+
+export { sendEmbed, defaultEmbed };
